64160617244b8cbd2674fe134102df93f1aa0cf7
Rod Mcnew <rodmcnew@gmail.com>
2015-09-07 13:43:36 -0500

    clean job ui

--- job.js
+++ job.js
@@ -1,14 +1,17 @@
-var module = angular.module('UrlRunner', []);
+var module = angular.module('UrlRunner', ['ui.bootstrap.datetimepicker']);
 
-module.controller('Job', function ($http) {
+module.controller('Job', function ($http, $scope) {
     var self = this;
     var jobs = [];
-    function ajaxError(err){
-        console.error('ajax error',err);
+
+    function ajaxError(err) {
+        console.error('ajax error', err);
     }
-    $http.get('http://api.urlrunner.com/api/account/1/job').success(
-        function (data) {
-            jobs = data;
-        }).error(ajaxError);
+
+    $scope.job = {nextRunTime: 'Thu Jan 01 2015 01:00:00 GMT-0600 (CST)'};
+    //$http.get('http://api.urlrunner.com/api/account/1/job').success(
+    //    function (data) {
+    //        jobs = data;
+    //    }).error(ajaxError);
 })
 ;
